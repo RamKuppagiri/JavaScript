e1f2fd719c570f0b802d7920f174210a4f60046e
Durga Bhavani Kuppagiri <bhavani.kuppagiri@gmail.com>
2023-05-17 16:04:08 -0500

    learning git commands

---
+++ day1.js
@@ -0,0 +1,28 @@
+// hello , just learning git commands
+/**
+ * 
+ *  This is multiline comment
+ * 
+ * git add . -> which adds all the local changes to the stage
+ * 
+ * git status -> to verify all files are committed/ if anything else need to be committed
+ * 
+ * git commit -m 'learning git commands'
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
