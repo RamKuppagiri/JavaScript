fc42d94b69ee80802f66678685fdbc2524914d8e
RamKuppagiri <kramfl5@gmail.com>
2023-05-17 21:39:32 -0500

    js basics

--- day1.js
+++ day1.js
@@ -28,3 +28,120 @@
  * 
  * 
  */
+
+
+
+// variables
+
+// let, const
+let a = 5;
+const b = 4;
+
+b = a+b;
+console.log(b);
+
+
+// data types
+let names = 'Ram';
+
+
+// 'C' // -> character
+// 'ABCD' // -> STRING
+//  1, 1.5 // -> number
+//  'A1@' // -> String
+//  true, false // -> boolean
+
+//  const ab = true;
+
+// data structure
+// array ->  array is collection of similar type of elements
+const fruits = ['mango', 'apple', 'guava'];
+
+const ages = [1,4,34,40];
+
+// family 
+
+const dadAge = 55;
+const momAge = 48;
+const ramAge = 29;
+const bhavaniAge = 27;
+
+const familyAge = [55,48,29,27];
+
+const familyArray = [{name: 'ram',
+age: 23,
+address: '318 west, tx, 78634',
+height: 6,
+weight: 20,
+favColors: ['red', 'yellow', 'orange'],
+favNums: [1,2,3,]},
+{name: 'bhavani',
+age: 23,
+address: '318 west, tx, 78634',
+height: 6,
+weight: 20,
+favColors: ['red', 'yellow', 'orange'],
+favNums: [1,2,3,]},
+{name: 'prabhavathi',
+age: 23,
+address: '318 west, tx, 78634',
+height: 6,
+weight: 20,
+favColors: ['red', 'yellow', 'orange'],
+favNums: [1,2,3,]},
+{name: 'Ramarao',
+age: 23,
+address: '318 west, tx, 78634',
+height: 6,
+weight: 20,
+favColors: ['red', 'yellow', 'orange'],
+favNums: [1,2,3,]}];
+
+// javascript obj
+
+const familyObj = {
+ dadAge : 55,
+ momAge : 48,
+ ramAge : 29,
+ bhavaniAge : 27,
+}
+
+// person Data
+const ramsBio = {
+    name: 'ram',
+    age: 23,
+    address: '318 west, tx, 78634',
+    height: 6,
+    weight: 20,
+    favColors: ['red', 'yellow', 'orange'],
+    favNums: [1,2,3,]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
