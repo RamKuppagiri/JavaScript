972ccd1ea9f8b5a923574388bbdc49d39928f9dc
RamKuppagiri <kramfl5@gmail.com>
2023-05-21 20:32:02 -0500

    commonFunctions.js

---
+++ commonFunctions.js
@@ -0,0 +1,16 @@
+/**
+ * addition function definition 
+ * @param {number} num1
+ * @param {number} num2 
+ */
+ exports.addition = (num1,num2) => console.log(num1+num2);
+  
+
+/**
+ * substraction function definition
+ * @param {number} num1 
+ * @param {number} num2 
+ */
+exports.substraction = (num1,num2) => console.log(num2-num1);
+
+
