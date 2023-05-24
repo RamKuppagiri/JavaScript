40103b00de06480facf9c3fff3edda9d122cdcba
RamKuppagiri <kramfl5@gmail.com>
2023-05-23 21:28:20 -0500

    function imolementation for divison & multiplication

--- commonFunctions.js
+++ commonFunctions.js
@@ -14,3 +14,20 @@
 exports.substraction = (num1,num2) => console.log(num2-num1);
 
 
+/**
+ * multiplication function definition 
+ * @param {number} num1
+ * @param {number} num2 
+ */
+exports.multiplication = (num1,num2) => console.log(num1*num2);
+  
+
+/**
+ * division function definition
+ * @param {number} num1 
+ * @param {number} num2 
+ */
+exports.division = (num1,num2) => console.log(num2%num1);
+
+
+
